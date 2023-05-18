e440d953e4a961fde0ee0c1aa3edc17dcf91ce39
mstafahasan <21p0349@eng.asu.edu.eg>
2023-05-18 18:53:05 +0300

    Update models.py

--- AsnanDental/website/models.py
+++ AsnanDental/website/models.py
@@ -18,6 +18,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=100)
     subject = models.CharField(max_length=100)
     message = models.TextField()
-    
+
+    def __str__(self):
+        return self.name     
+        
 
 
