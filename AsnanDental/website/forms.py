652f43d4c2d9e56258d9ad7204a20da5b505b972
adelmohamed11 <129177338+adelmohamed11@users.noreply.github.com>
2023-05-18 18:57:07 +0300

    Update forms.py

--- AsnanDental/website/forms.py
+++ AsnanDental/website/forms.py
@@ -3,7 +3,8 @@ from django import forms
 from .models import Appointment, Contact
 
 class AppointmentForm(forms.ModelForm):
-   
+    class Meta:
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
