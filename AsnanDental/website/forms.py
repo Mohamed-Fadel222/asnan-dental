cb220accc3eeec89fdcc5d844b6f74a4e83155d6
adelmohamed11 <129177338+adelmohamed11@users.noreply.github.com>
2023-05-18 18:46:30 +0300

    Update forms.py

--- AsnanDental/website/forms.py
+++ AsnanDental/website/forms.py
@@ -6,3 +6,8 @@ class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = '__all__'
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
