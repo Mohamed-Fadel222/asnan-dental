40d9b2a92b3bb92ba88294c0989d6473f181e711
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2023-05-18 18:52:08 +0300

    url.py(price)

--- AsnanDental/website/urls.py
+++ AsnanDental/website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact.html', views.contact, name='contact'),
     path('about.html', views.about, name='about'),
+    path('price.html', views.price, name='price'),
 
 ]
