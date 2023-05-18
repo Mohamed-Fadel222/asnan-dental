6912965b0fe71a57b39e79b555aeb4fa9339c114
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2023-05-18 18:51:14 +0300

    url.py (about)

--- AsnanDental/website/urls.py
+++ AsnanDental/website/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact.html', views.contact, name='contact'),
+    path('about.html', views.about, name='about'),
 
 ]
