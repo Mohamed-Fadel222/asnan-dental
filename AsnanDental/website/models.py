61df7ce334a33a46467512de406a7d7bd613013c
mstafahasan <21p0349@eng.asu.edu.eg>
2023-05-18 18:48:59 +0300

    Update models.py

--- AsnanDental/website/models.py
+++ AsnanDental/website/models.py
@@ -5,4 +5,7 @@ from django.db import models
 
 class Appointment(models.Model):
     name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    date = models.DateField(null=True)
+
 
