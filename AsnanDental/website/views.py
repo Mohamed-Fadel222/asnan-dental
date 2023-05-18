e1f8b62c1a06f7beff51614217e1f4bc433461bc
BaselAshraf81 <129132034+BaselAshraf81@users.noreply.github.com>
2023-05-18 19:05:46 +0300

    update views

--- AsnanDental/website/views.py
+++ AsnanDental/website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Contact, Appointment
 from .forms import AppointmentForm, ContactForm
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
