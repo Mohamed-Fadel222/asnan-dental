29c3c68935a75759b7f10982db837c6c9c92abe9
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2023-05-18 18:12:16 +0300

    main.js

--- AsnanDental/static/website/js/main.js
+++ AsnanDental/static/website/js/main.js
@@ -10,3 +10,17 @@
         }, 1);
     };
     spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.navbar').addClass('sticky-top');
+        } else {
+            $('.navbar').removeClass('sticky-top');
+        }
+    });
