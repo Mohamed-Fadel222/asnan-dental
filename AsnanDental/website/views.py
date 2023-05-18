af32cbd51505837394e04e070d3a1ff9c807b8de
Ahmed Khafagy <91484780+AhmedKhafagyy@users.noreply.github.com>
2023-05-18 19:01:29 +0300

    importing in views.py

--- AsnanDental/website/views.py
+++ AsnanDental/website/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Contact, Appointment
+from .forms import AppointmentForm, ContactForm
 # Create your views here.
