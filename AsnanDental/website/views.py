d56762bb21bb436b68dc423f2e7a861eefcefd62
BaselAshraf81 <129132034+BaselAshraf81@users.noreply.github.com>
2023-05-18 19:05:57 +0300

    update views

--- AsnanDental/website/views.py
+++ AsnanDental/website/views.py
@@ -5,3 +5,12 @@ from .forms import AppointmentForm, ContactForm
 def index(request):
     return render(request, 'index.html')
 
+
+def contact(request):
+    if request.POST:
+        dataform = ContactForm(request.POST)
+        if dataform.is_valid():
+            dataform.save()
+        return redirect(contact)
+    # name = request.POST['message-name']
+    return render(request, 'contact.html', {'dataform': ContactForm})
